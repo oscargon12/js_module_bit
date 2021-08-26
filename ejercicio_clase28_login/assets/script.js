293247661de62887eff667d4687305974308250c
oscargon12 <oscargon12@gmail.com>
2021-08-25 23:13:07 -0500

    subiendo ejercicio mongodb practica.mongodb

--- ejercicio_clase28_login/assets/script.js
+++ ejercicio_clase28_login/assets/script.js
@@ -1,77 +1,51 @@
 console.log('Traer a Morty');
 
-const showCards = () => {
 
-    // Traigo el valor del input como la variable numChar
-    let numChar = parseInt(document.querySelector('#charNumber').value);
-    console.log(numChar);
+// Primera función: recibe el API, con un for defino hasta cuentos muestra, retorna data del api
+const showCards = () =>{
+  
+  // Traigo el valor del input como la variable numChar
+  let numchar = parseInt(document.querySelector('#charNumber').value)
+  console.log(`Se muestran ${numchar} personajes`);
 
-    document.querySelector('#cardContainer').innerHTML = ''
+  document.querySelector('#cardContainer').innerHTML = '' // resetea las tarjetaspara que no se sumen
 
-    // NumChar es el límite del ciclo, osea va hasta lo que ingresó el usuario
-    for(let i = 1; i <= numChar; i++){
+
+  for(let i = 1; i <= numchar; i++){
       let url = `https://rickandmortyapi.com/api/character/${i}`;
-    
-        // Fetch
-        fetch(url)
-            .then((res) => res.json()) // Recibe un json y retorna un valor que recibe el siguiente .then
-            .then((data) => print(data)) // Recibe la data de la petición
-            .catch((err) => console.log(err));
-    }
-
-
-    
-    if(numChar === 0){
-        console.log('No hay nada que mostrar');
-
-        card = `
-        <div class="alert alert-danger" role="alert">
-            No hay nada que mostrar
-        </div>
-        `;
-      document.querySelector('#cardContainer').innerHTML = card
-    }
-
-    const print = (data) => {
-        
-      if(numChar === 1){
-        card = `
-        <div class="col-12">
-            <div class="card m-2 p-0">
-                <img src="${data.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${data.name}</h5>
-                  <h6 class="card-title">ID: ${data.id}</h6>
-                  <p class="card-text"><b>Especie: </b>${data.species}</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        `;
-      } else {
-
-        console.log(data);
-        card = `
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="card m-2 p-0">
-                <img src="${data.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${data.name}</h5>
-                  <h6 class="card-title">ID: ${data.id}</h6>
-                  <p class="card-text"><b>Especie: </b>${data.species}</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        `;
-      }
-      
-    
-      document.querySelector('#cardContainer').innerHTML += card
-    }
-
-    document.querySelector('#charNumber').value = '';
+  
+      // Fetch
+      fetch(url)
+          .then((res) => res.json()) // Recibe un json y retorna 
+          .then((data) => print(data)) // Recibe la data de la petición
+          .catch((err) => console.log(err));
+  }
 }
+
+
+
+const print = (data) => {
+  // let card = '';
+
+    let card = `
+    <div class="col-12 col-sm-6 col-md-4">
+        <div class="card m-1 p-0">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${data.name}</h5>
+              <h6 class="card-title">ID: ${data.id}</h6>
+              <p class="card-text"><b>Especie: </b>${data.species}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    `;
+
+  document.querySelector('#cardContainer').innerHTML += card
+}
+
+
+
 
  // == Ocultando tarjetas ==
  const hideCards = () => {
